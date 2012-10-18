508ea58674c3e37f2298b99e1680c587a07dc7ab
Unbounded <unbounded@Laptops-MacBook-Pro.local>
2012-10-18 12:53:19 -0400

    Init commit

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Unbounded on 10/18/12.
+//  Copyright (c) 2012 Cory Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Unbounded on 10/18/12.
+//  Copyright (c) 2012 Cory Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
